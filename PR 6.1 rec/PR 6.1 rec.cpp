6cd851cc8d159a58826aa1ba66ad9ebcc7ff92e7
ivanbotsko <ivan.botsko.ik.2023@lpnu.ua>
2023-10-31 19:24:19 +0200

    first master commit

---
+++ PR 6.1 rec/PR 6.1 rec.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+
+std::vector<int> generateArray(int size, int min, int max) {
+    std::vector<int> array;
+    for (int i = 0; i < size; i++) {
+        array.push_back(rand() % (max - min + 1) + min);
+    }
+    return array;
+}
+
+struct CalculationResult {
+    int count;
+    int sum;
+    std::vector<int> array;
+};
+
+CalculationResult calculateAndReplace(std::vector<int>& array, int i = 0, int count = 0, int sum = 0) {
+    if (i < array.size()) {
+        if (array[i] <= 0 || i % 2 == 0) {
+            count++;
+            sum += array[i];
+            array[i] = 0;
+        }
+        return calculateAndReplace(array, i + 1, count, sum);
+    }
+    else {
+        return { count, sum, array };
+    }
+}
+
+int main() {
+    srand(static_cast<unsigned>(time(nullptr)));
+
+    std::vector<int> array = generateArray(26, -40, 20);
+    std::cout << "Original array: ";
+    for (int i = 0; i < array.size(); i++) {
+        std::cout << array[i];
+        if (i != array.size() - 1) {
+            std::cout << ", ";
+        }
+    }
+    std::cout << std::endl;
+
+    CalculationResult result = calculateAndReplace(array);
+    std::cout << "Number of elements that satisfy the condition: " << result.count << std::endl;
+    std::cout << "Sum of elements that satisfy the condition: " << result.sum << std::endl;
+
+    std::cout << "Modified array: ";
+    for (int i = 0; i < result.array.size(); i++) {
+        std::cout << result.array[i];
+        if (i != result.array.size() - 1) {
+            std::cout << ", ";
+        }
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <vector>
+#include "../PR 6.1 rec/PR 6.1 rec.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+TEST_CLASS(GenerateArrayTest) {
+public:
+    TEST_METHOD(GenerateArrayGeneratesArrayOfCorrectSize) {
+        int size = 26;
+        int min = -20;
+        int max = 40;
+        std::vector<int> array = generateArray(size, min, max);
+        Assert::AreEqual(size, static_cast<int>(array.size()));
+    }
+
+    TEST_METHOD(GenerateArrayGeneratesArrayWithinSpecifiedRange) {
+        int size = 20;
+        int min = -20;
+        int max = 40;
+        std::vector<int> array = generateArray(size, min, max);
+        for (int element : array) {
+            Assert::IsTrue(element >= min && element <= max);
+        }
+ 
+    }
+};
